68ad63b529d6222fc6bd0e03f3e42cea16dd965c
kentatano <kenta43527532@gmail.com>
2025-07-17 17:34:34 +0900

    修正

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(() => {
-        document.getElementById("splash").style.display = "none";
-        document.getElementById("mainContent").style.display = "block";
-    }, 3000);
-});
